e7991c8e76bae8f1d2d195bc6aa5abccd2e92dae
IvanStoykov04 <ivanstoikov2004.15@gmail.com>
2024-05-07 18:28:50 +0300

    added some new rows

--- ProceduresCursorstrigers/ProceduresCursorTtrigers.sql
+++ ProceduresCursorstrigers/ProceduresCursorTtrigers.sql
@@ -28,3 +28,36 @@ END;
 DELIMITER ;
 CALL coachCursor();
 
+-- triger
+CREATE TABLE Student(
+id INT AUTO_INCREMENT PRIMARY KEY,
+id_student INT NOT NULL,
+name VARCHAR(100) NOT NULL,
+egn VARCHAR(10) NOT NULL,
+phone VARCHAR(10) NOT NULL
+);
+
+
+CREATE TABLE newStudent(
+id INT AUTO_INCREMENT PRIMARY KEY,
+event ENUM('INSERT','UPDATE','DELETE') NOT NULL,
+old_id_student INT NOT NULL,
+new_id_student INT NOT NULL,
+old_name VARCHAR(100) NOT NULL,
+new_name VARCHAR(100) NOT NULL,
+old_egn VARCHAR(10) NOT NULL,
+new_egn VARCHAR(10) NOT NULL,
+old_phone VARCHAR(10) NOT NULL,
+new_phone VARCHAR(10) NOT NULL,
+dateOfLog DATETIME
+):
+
+DELIMITER |
+CREATE TRIGGER triger AFTER UPDATE ON student
+FOR EACH ROW
+BEGIN
+INSERT INTO newStudent(event,old_id_student,new_id_student,old_name,new_name,old_egn,new_egn,old_phone,new_phone,dateOfLog)
+VALUES('UPDATE',OLD.id_student,NEW.id_student,OLD.name,NEW.name,OLD.egn,NEW.egn,OLD.phone,NEW.phone,NOW());
+END;
+|
+DELIMITER ;
